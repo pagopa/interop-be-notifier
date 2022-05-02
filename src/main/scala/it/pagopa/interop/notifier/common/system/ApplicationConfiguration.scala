99b8270035655181a94f93d4a88ef6af902abb8a
P3trur0 <ubaldopescatore@gmail.com>
2022-05-02 15:02:11 +0200

    added audience

--- src/main/scala/it/pagopa/interop/notifier/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/interop/notifier/common/system/ApplicationConfiguration.scala
@@ -2,12 +2,9 @@ package it.pagopa.interop.notifier.common.system
 
 import com.typesafe.config.{Config, ConfigFactory}
 
-import scala.jdk.CollectionConverters.CollectionHasAsScala
-
 object ApplicationConfiguration {
   val config: Config                 = ConfigFactory.load()
   val serverPort: Int                = config.getInt("notifier.port")
-  val interopAudience: Set[String]   = config.getStringList("notifier.jwt.audience").asScala.toSet
   val rsaPrivatePath: String         = config.getString("notifier.rsa-private-path")
   val agreementManagementURL: String = config.getString("notifier.services.agreement-management")
   val purposeManagementURL: String   = config.getString("notifier.services.purpose-management")
@@ -15,4 +12,9 @@ object ApplicationConfiguration {
   val dynamoTableName: String        = config.getString("notifier.dynamo.table-name")
   val queueURL: String               = config.getString("notifier.queue.url")
   val threadPoolSize: Int            = config.getInt("notifier.queue.thread-pool-size")
+
+  val interopAudience: Set[String] =
+    config.getString("notifier.jwt.audience").split(",").toSet.filter(_.nonEmpty)
+
+  require(interopAudience.nonEmpty, "Audience cannot be empty")
 }

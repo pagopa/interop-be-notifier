4f6ed9c66428e98d708bcb9b89c92097c50df6f9
galales <25105748+galales@users.noreply.github.com>
2022-06-01 11:47:01 +0200

    0.1.x: Update deps version

--- project/Versions.scala
+++ project/Versions.scala
@@ -24,8 +24,8 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val commonsVersion             = "0.1.x-SNAPSHOT"
-  lazy val purposeManagementVersion   = "0.1.x-SNAPSHOT"
-  lazy val agreementManagementVersion = "0.1.x-SNAPSHOT"
-  lazy val catalogManagementVersion   = "0.1.x-SNAPSHOT"
+  lazy val commonsVersion             = "0.1.8"
+  lazy val purposeManagementVersion   = "0.1.5"
+  lazy val agreementManagementVersion = "0.1.5"
+  lazy val catalogManagementVersion   = "0.1.5"
 }

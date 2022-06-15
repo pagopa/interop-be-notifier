3aca004df8193bf414c865a279fe84d222a532ba
Stefano Perazzolo <stefano.perazzolo@gmail.com>
2022-06-15 20:02:32 +0200

    0.1.x: Bump to 1.0.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -23,8 +23,8 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val commonsVersion             = "0.1.x-SNAPSHOT"
-  lazy val purposeManagementVersion   = "0.1.x-SNAPSHOT"
-  lazy val agreementManagementVersion = "0.1.x-SNAPSHOT"
-  lazy val catalogManagementVersion   = "0.1.x-SNAPSHOT"
+  lazy val commonsVersion             = "1.0.0"
+  lazy val purposeManagementVersion   = "1.0.0"
+  lazy val agreementManagementVersion = "1.0.0"
+  lazy val catalogManagementVersion   = "1.0.0"
 }

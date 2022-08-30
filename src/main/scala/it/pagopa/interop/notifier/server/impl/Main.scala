b1376a37405ef5c2ef0f301636e8dfc9fbe6c481
galales <25105748+galales@users.noreply.github.com>
2022-08-30 14:38:29 +0200

    pin-1771-fix: Init projections in main

--- src/main/scala/it/pagopa/interop/notifier/server/impl/Main.scala
+++ src/main/scala/it/pagopa/interop/notifier/server/impl/Main.scala
@@ -55,6 +55,8 @@ object Main extends App with CORSSupport with Dependencies {
 
       cluster.subscriptions ! Subscribe(listener, classOf[ClusterEvent.MemberEvent])
 
+      if (ApplicationConfiguration.projectionsEnabled) initProjections()
+
       logger.info(renderBuildInfo(BuildInfo))
       logger.info(s"Started cluster at ${cluster.selfMember.address}")
 

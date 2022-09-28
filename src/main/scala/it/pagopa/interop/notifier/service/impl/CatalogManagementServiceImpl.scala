d8031cfdff53468990ee7a05614c51bf2680b2d2
Antonio Gelameris <toniogela89@gmail.com>
2022-09-28 17:00:57 +0200

    Adapted to new commons

--- src/main/scala/it/pagopa/interop/notifier/service/impl/CatalogManagementServiceImpl.scala
+++ src/main/scala/it/pagopa/interop/notifier/service/impl/CatalogManagementServiceImpl.scala
@@ -2,8 +2,7 @@ package it.pagopa.interop.notifier.service.impl
 
 import it.pagopa.interop.catalogmanagement.client.invoker.BearerToken
 import it.pagopa.interop.catalogmanagement.client.model.EService
-import it.pagopa.interop.commons.utils.TypeConversions.EitherOps
-import it.pagopa.interop.commons.utils.extractHeaders
+import it.pagopa.interop.commons.utils.withHeaders
 import it.pagopa.interop.notifier.service.{CatalogManagementApi, CatalogManagementInvoker, CatalogManagementService}
 import com.typesafe.scalalogging.{Logger, LoggerTakingImplicit}
 import it.pagopa.interop.commons.logging.{CanLogContextFields, ContextFieldsToLog}
@@ -19,13 +18,12 @@ final class CatalogManagementServiceImpl(invoker: CatalogManagementInvoker, api:
     Logger.takingImplicit[ContextFieldsToLog](this.getClass)
 
   private[this] def getEServiceById(eServiceId: UUID)(implicit contexts: Seq[(String, String)]): Future[EService] =
-    for {
-      (bearerToken, correlationId, ip) <- extractHeaders(contexts).toFuture
-      request = api.getEService(xCorrelationId = correlationId, eServiceId.toString, xForwardedFor = ip)(
+    withHeaders { (bearerToken, correlationId, ip) =>
+      val request = api.getEService(xCorrelationId = correlationId, eServiceId.toString, xForwardedFor = ip)(
         BearerToken(bearerToken)
       )
-      result <- invoker.invoke(request, s"Retrieving EService $eServiceId")
-    } yield result
+      invoker.invoke(request, s"Retrieving EService $eServiceId")
+    }
 
   override def getEServiceProducerByEServiceId(eServiceId: UUID)(implicit
     contexts: Seq[(String, String)]

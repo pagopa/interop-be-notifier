999771053b73107822c19115e52e3da054010663
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2022-12-13 17:52:54 +0100

    pin-2210: Remove unused errors

--- src/main/scala/it/pagopa/interop/notifier/error/NotifierErrors.scala
+++ src/main/scala/it/pagopa/interop/notifier/error/NotifierErrors.scala
@@ -4,7 +4,6 @@ import it.pagopa.interop.commons.utils.errors.ComponentError
 
 object NotifierErrors {
 
-  final case object InternalServerError extends ComponentError("0001", "There was an internal server error")
   final case class OrganizationNotFound(organizationId: String)
       extends ComponentError("0002", s"Organization $organizationId not found")
 

7b0cea71b5531570019ab51929843401bba095c5
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2023-04-03 16:56:03 +0200

    Update pagopa versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -23,8 +23,8 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val commonsVersion             = "1.0.x-SNAPSHOT"
-  lazy val purposeManagementVersion   = "1.0.x-SNAPSHOT"
-  lazy val agreementManagementVersion = "1.0.x-SNAPSHOT"
-  lazy val catalogManagementVersion   = "1.0.x-SNAPSHOT"
+  lazy val commonsVersion             = "1.0.15"
+  lazy val purposeManagementVersion   = "1.0.7"
+  lazy val agreementManagementVersion = "1.0.9"
+  lazy val catalogManagementVersion   = "1.0.11"
 }
